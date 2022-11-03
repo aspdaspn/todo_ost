56948a01711bca3e05a8ff07128664f11cec36a5
Diego Casanova <casanord95@gmail.com>
2022-11-03 18:09:10 +0100

    Better fix for ListItem checkbox issue

--- src/components/controls/ListItem.tsx
+++ src/components/controls/ListItem.tsx
@@ -1,6 +1,5 @@
 import { Importance } from "models/Importance";
 import { Todo } from "models/Todo";
-import { useState } from "react";
 
 interface Props {
     todo: Todo,
@@ -9,22 +8,9 @@ interface Props {
 }
 
 export const ListItem = (props: Props) => {   
-
-    const [checked, setChecked] = useState(props.todo.done)
-
-    function updateDone(isDone: boolean) {
-        setChecked(isDone)
-        props.updateItem(props.todo.id, isDone)
-    }
-
-    function remove() {
-        setChecked(false) // fix to prevent that checkbox is checked for new ListItem at this position
-        props.removeItem(props.todo.id)
-    }
-
     return (
         <div className="task-entry ">
-            <input type="checkbox" name="done" className="done-checkbox" checked={checked} onChange={e => updateDone(e.target.checked)}/>
+            <input type="checkbox" name="done" className="done-checkbox" checked={props.todo.done} onChange={e => props.updateItem(props.todo.id, e.target.checked)}/>
             <label className="bolt" /*name="prio-1"*/>
                 <i className="fa-solid fa-bolt"></i>
             </label>
@@ -35,7 +21,7 @@ export const ListItem = (props: Props) => {
                 <i className="fa-solid fa-bolt"></i>
             </label>
             <label className={`task-description ${props.todo.done ? "strikethrough" : ""}`}>{props.todo.text}</label>
-            <button type="submit" className="button delete" name="delete" onClick={remove}>
+            <button type="submit" className="button delete" name="delete" onClick={() => props.removeItem(props.todo.id)}>
                 Löschen
             </button>
         </div>
